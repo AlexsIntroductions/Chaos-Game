5ed00e0de76fa40f566b94c79589d6e0fbbd67a0
AlexsIntroductions <alexsintroductions@gmail.com>
2022-09-14 15:21:43 -0400

    Reset Button, Finalized Logic, Bootstrap Support

--- script.js
+++ script.js
@@ -1,41 +1,47 @@
-var Canvas = document.getElementById("myCanvas");
+var canvas = document.getElementById("myCanvas");
 //instances 2d on it
-var ctx = Canvas.getContext("2d");
+var ctx = canvas.getContext("2d");
 
-ctx.fillStyle = 'black';
+window.addEventListener('resize', () => {
+	setCanvasSize();
+});
 
 //Variables
 var pointCoords = new Array(3);
 var i = 0;
-var currentPoint;
+var currentPoint = null;
+let t = 50;
+let requestID = new Array(t);
 
 
 //---------------OBJECTS---------------//
-function Coord(x, y){
+function Coord(x, y) {
 	this.x = x;
 	this.y = y;
 }
 
 //---------------OBJECT FUNCTIONS---------------//
-Coord.prototype.draw = function(n){
+Coord.prototype.draw = function (n) {
 	ctx.beginPath();
 	ctx.arc(this.x, this.y, n, 0, 2 * Math.PI);
 	ctx.fill();
 }
 
 //---------------HELPER FUNTIONS---------------//
-Canvas.addEventListener("mousedown", function(e){
-	onClick(Canvas, e);
+canvas.addEventListener("mousedown", function (e) {
+	onClick(canvas, e);
 });
 
-function setCanvasSize(){
-	Canvas.width = 1750;
-	Canvas.height = 800;
+
+function setCanvasSize() {
+	canvas.width = parent.innerWidth * 7 / 8;
+	canvas.height = canvas.width * 2 / 3;
 }
 
-function onClick(smallCanvas, event){
-	if(i < 3){
-		let rect = Canvas.getBoundingClientRect();
+function onClick(smallcanvas, event) {
+	if (i < 3) {
+		ctx.fillStyle = 'black';
+		let rect = canvas.getBoundingClientRect();
 		let x = Math.floor(event.clientX - rect.left);
 		let y = Math.floor(event.clientY - rect.top);
 		console.log(x + ", " + y);
@@ -44,62 +50,72 @@ function onClick(smallCanvas, event){
 		pointCoords[i].draw(5);
 		i++;
 	}
-	else if(i == 3){
-		let rect = Canvas.getBoundingClientRect();
+	else if (i == 3) {
+		ctx.fillStyle = "red";
+		let rect = canvas.getBoundingClientRect();
 		let x = Math.floor(event.clientX - rect.left);
 		let y = Math.floor(event.clientY - rect.top);
 		currentPoint = new Coord(x, y);
-		currentPoint.draw(0.1);
+		currentPoint.draw(1.0);
 		i++;
+		ctx.fillStyle = 'black';
 	}
 }
 
-function action(){
-	//change rng to eb any number of points
-	
-	//change the half to change based on what fractal you are doing
-		//half will just be one of the options
-	
-	
-	
+function action() {
 	//pick point 1 - 3
 	var point = Math.floor(Math.random() * 3);
 	//go halfway between current point and picked poink
-	var half = new Coord( (pointCoords[point].x + currentPoint.x) / 2, (pointCoords[point].y + currentPoint.y) / 2 );
-	currentPoint = half;
+	currentPoint = new Coord((pointCoords[point].x + currentPoint.x) / 2, (pointCoords[point].y + currentPoint.y) / 2);
 	//draw point there and set that to new point
 	currentPoint.draw(0.1);
-	//repeat
 }
 
-function start(){
-	for(let i = 0; i < 1000; i++){
-		step();
+function start() {
+	if(currentPoint === null){
+		return;
+	}
+	for(let i = 0; i < t; i++){
+		requestID[i] = requestAnimationFrame(function(){
+			step(i);
+		});
 	}
 }
 
-function step() {
+function step(i) {
 	action();
-	window.requestAnimationFrame(step);
+	requestID[i] = requestAnimationFrame(function(){
+		step(i);
+	});
 }
 
-//Presets?
-	//1750 x 800
-function equalateral(){
-	pointCoords[0] = new Coord(1750 / 2, 0);
-	pointCoords[1] = new Coord((1750 / 2) + (1750 / (3 + (2/3))), 800);
-	pointCoords[2] = new Coord((1750 / 2) - (1750 / (3 + (2/3))), 800);
+function reset(){
+	for(let i = 0; i < t; i++){
+		cancelAnimationFrame(requestID[i]);
+	}
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	currentPoint = null;
+	i = 0;
+}
+
+//Presets
+function equalateral() {
+	pointCoords[0] = new Coord(canvas.width / 2, (canvas.height * 1/8));
+	pointCoords[1] = new Coord((canvas.width / 2) + (canvas.width / (3 + (2 / 3))), (canvas.height * 2 / 3) + (canvas.height * 1/8));
+	pointCoords[2] = new Coord((canvas.width / 2) - (canvas.width / (3 + (2 / 3))), (canvas.height * 2 / 3) + (canvas.height * 1/8));
 	pointCoords[0].draw(5);
 	pointCoords[1].draw(5);
 	pointCoords[2].draw(5);
-	
-	currentPoint = new Coord( (pointCoords[0].x + pointCoords[1].x) / 2, (pointCoords[0].y + pointCoords[1].y) / 2 );
+
+	currentPoint = new Coord((pointCoords[0].x + pointCoords[1].x) / 2, (pointCoords[0].y + pointCoords[1].y) / 2);
 	currentPoint.draw(0.1);
 	i = 4;
 }
 
-function square(){
-	
+function square() {
+
 }
 
+
+//-----------Initiation
 setCanvasSize();
